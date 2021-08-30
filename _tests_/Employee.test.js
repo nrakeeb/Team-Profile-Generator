6363f408da4dfac73ea6bec1028b16ccda620d3d
Nouha Rakeeb <Nouha_rakeeb@hotmail.co.uk>
2021-08-30 23:44:34 +0100

    test: added employee tests
    Authors: NR

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,17 @@
+const Employee = require("../lib/Employee")
+
+const employee = new Employee("Nouha", 1, "Nouha@hotmail.com")
+
+describe("Employee", () => {
+    it("should have all correct values when ", () => {
+        expect(employee.name).toBe("Nouha");
+        expect(employee.id).toBe(1);
+        expect(employee.email).toBe("Nouha@hotmail.com");
+    });
+    it("should return all correct values when getter methods are called", () => {
+        expect(employee.getName()).toBe("Nouha");
+        expect(employee.getId()).toBe(1);
+        expect(employee.getEmail()).toBe("Nouha@hotmail.com");
+        expect(employee.getRole()).toBe("Employee");
+    });
+});
